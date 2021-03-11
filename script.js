757520d0050e270ab8ddf452785c5439992d651b
MSebb23 <matteosabbatini23@gmail.com>
2021-03-11 01:53:44 +0100

    Completed JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var calculate = document.getElementById('calculate');
 var burger_name = document.getElementById('burger_name');
-var ingredients = document.getElementsByClassName('add_ingredient');
+var ingredients = document.getElementsByClassName('add_ingredients');
 var coupon = document.getElementById('discount');
 var coupon_codes = ["abc123", "Potato Salad", "123ABC"];
 
@@ -10,14 +10,14 @@ calculate.addEventListener( "click",
         if (burger_name.value != "") {
             var price = 50;
             console.log(price);
-            console.log(ingredients[i]);
             // cicla sugli ingredienti e se l'ingrediente è selezionato, somma i value nella variabile price
             for (var i = 0; i < ingredients.length; i++ ) {
 
+                console.log("inside for", price, i, ingredients[i]);
 
                 if (ingredients[i].checked == true) {
                     price += parseInt(ingredients[i].value) 
-                    console.log(price)
+                    console.log("inside if", price);
 
                 }
             }
